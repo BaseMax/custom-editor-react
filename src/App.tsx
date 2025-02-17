1ada35f56fb8143234069b04e3a29bb5a195412e
Max Base <MaxBaseCode@gmail.com>
2025-02-17 16:21:20 +0330

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from 'react'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import ReactMarkdown from "react-markdown";
 
-function App() {
+const socket = io("http://localhost:3001");
+
+const App = () => {
+  const [markdown, setMarkdown] = useState("");
+
+  useEffect(() => {
+    socket.on("updateMarkdown", (content) => {
+      setMarkdown(content);
+    });
+
+    return () => {
+      socket.off("updateMarkdown");
+    };
+  }, []);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const content = e.target.value;
+    setMarkdown(content);
+    socket.emit("markdownChange", content);
+  };
 
   return (
-    <>
-    </>
-  )
-}
+    <div className="flex h-screen">
+      {/* Editor */}
+      <textarea
+        value={markdown}
+        onChange={handleChange}
+        placeholder="Type your markdown here..."
+        className="w-1/2 h-full p-4 text-lg border-r border-gray-300 resize-none"
+      />
 
-export default App
+      {/* Live Preview */}
+      <div className="w-1/2 h-full p-4 bg-gray-100 overflow-auto">
+        <ReactMarkdown>{markdown}</ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default App;
